d6a8bb3af99f787f282fa1c7efa8612c702c0a5e
wrt6000 <wrt6000@hotmail.com>
2016-09-19 03:08:57 -0500

    avances generales

--- app/src/main/java/com/grupov08/easyfood_domiciliario/MainActivity.java
+++ app/src/main/java/com/grupov08/easyfood_domiciliario/MainActivity.java
@@ -1,40 +1,91 @@
 package com.grupov08.easyfood_domiciliario;
 
+
+import android.content.Intent;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    public final static String numerotel = "telefonoActual.txt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public void realizarPedido (View v)
-    {
-        Toast.makeText(MainActivity.this, "Btn Realizar Pedido", Toast.LENGTH_SHORT).show();
+        String telString = "";
+        String nomdomi = "";
         try
         {
-            SmsManager.getDefault().sendTextMessage("123456",null,"Prueba Mensaje",null,null);
+
+            File f =  new File(Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_PICTURES), numerotel);
+            FileReader fr;
+            fr = new FileReader(f);
+            BufferedReader br = new BufferedReader(fr);
+            telString = br.readLine();
+            nomdomi = br.readLine();
+            br.close();
+        }
+        catch (IOException e) {
+            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+
+        ((EditText)findViewById(R.id.telefono_actual_main)).setText(telString);
+        ((EditText)findViewById(R.id.nombre_actual_domi)).setText(nomdomi);
+    }
+
+    public void contactarCliente (View v)
+    {
+        String infoDomiciliario = "Lo que se le mande al Cliente, teléfono Domiciliario, estado del pedido, distancia";
+        try
+        {
+            SmsManager.getDefault().sendTextMessage( ((EditText)findViewById(R.id.telefono_cliente)).getText().toString(),null,infoDomiciliario,null,null);
         }
         catch (Exception e)
         {
-            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            Toast.makeText(MainActivity.this, "El mensaje no pudo ser enviado", Toast.LENGTH_SHORT).show();
         }
-
+        Intent i = new Intent(this, PedidosActivity.class);
+        i.putExtra("telCliente",((EditText)findViewById(R.id.telefono_cliente)).getText().toString());
+        startActivity(i);
     }
 
-    public void pedidosAnteriores (View v)
+    public void cambiarTelefono(View v)
     {
-        Toast.makeText(MainActivity.this, "Btn Pedidos Anteriores", Toast.LENGTH_SHORT).show();
+        this.deleteFile(numerotel);
+        String str1 = ((EditText)findViewById(R.id.telefono_actual_main)).getText().toString();
+        String str2 = ((EditText)findViewById(R.id.nombre_actual_domi)).getText().toString();
+        FileOutputStream outputStream;
+        File file = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), numerotel);
+        try
+        {
+            PrintWriter out = new PrintWriter( file );
+            out.println(str1);
+            out.println(str2);
+            out.close();
+        }
+        catch (Exception e)
+        {
+        Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
     }
 
-    public void iniciarSesion (View v)
-    {
-        Toast.makeText(MainActivity.this, "Btn Iniciar Sesión", Toast.LENGTH_SHORT).show();
-    }
+
+
 }
--- app/src/main/java/com/grupov08/easyfood_domiciliario/PedidosActivity.java
+++ app/src/main/java/com/grupov08/easyfood_domiciliario/PedidosActivity.java
@@ -1,13 +1,53 @@
 package com.grupov08.easyfood_domiciliario;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class PedidosActivity extends AppCompatActivity {
+
+    private String telCliente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pedidos);
+        ArrayList<String> estadoPedido = new ArrayList<String>();
+        estadoPedido.add("En cola");
+        estadoPedido.add("En preparación");
+        estadoPedido.add("En camino");
+        Spinner spPedido = (Spinner)findViewById(R.id.spinner_pedido);
+        spPedido.setAdapter(new ArrayAdapter(this, android.R.layout.simple_spinner_item, estadoPedido));
+        Intent i = getIntent();
+        telCliente = i.getStringExtra("telCliente");
+        ((TextView)findViewById(R.id.telefono_cliente)).setText(telCliente);
     }
+
+    public void sincronizarConCliente(View v)
+    {
+        Toast.makeText(PedidosActivity.this, telCliente, Toast.LENGTH_SHORT).show();
+    }
+
+    public void enviarMensaje()
+    {
+
+        String mensaje = ((EditText)findViewById(R.id.mensaje_cliente_pedido)).getText().toString();
+        SmsManager.getDefault().sendTextMessage(mensaje,null,infoDomiciliario,null,null);
+    }
+
+    public void finalizarPedido (View v)
+    {
+        finish();
+    }
+
+
 }
